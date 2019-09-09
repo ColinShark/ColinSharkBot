487efe44225ba6e9238ae1861f7d525a158b8426
ColinShark <nicolas@neht.xyz>
2019-09-09 12:14:25 +0200

    Remove the couch Add .vscode folder to gitignore

--- colinsharkbot/plugins/welcome.py
+++ colinsharkbot/plugins/welcome.py
@@ -3,12 +3,12 @@ from pyrogram import Filters, Message, Emoji
 from ..colinsharkbot import ColinSharkBot
 
 MENTION = '<a href="tg://user?id={}">{}</a>'
-MESSAGE = "Welcome to {}Pyrograms Lounge{}, {}"
+MESSAGE = "Welcome to {}Pyrograms Lounge, {}"
 
 @ColinSharkBot.on_message(Filters.new_chat_members & Filters.chat("pyrogramlounge"))
 def welcome_people(bot: ColinSharkBot, msg: Message):
     new_members = [MENTION.format(x.id, x.first_name) for x in msg.new_chat_members]
-    text = MESSAGE.format(Emoji.FIRE, Emoji.COUCH_AND_LAMP, " ".join(new_members))
+    text = MESSAGE.format(Emoji.FIRE, " ".join(new_members))
     msg.reply_animation(
         animation="CgADAgADdQUAAleAoUsgGRxWRjl6_xYE",
         caption=text
