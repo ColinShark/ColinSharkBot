2b9133667355c9f99b1891a9fa61118de062cba2
ColinShark <nicolas@neht.xyz>
2019-09-12 12:43:42 +0200

    React to mentioning commands

--- colinsharkbot/plugins/no.py
+++ colinsharkbot/plugins/no.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 from ..colinsharkbot import ColinSharkBot
 
 
-@ColinSharkBot.on_message(Filters.command("no"))
+@ColinSharkBot.on_message(Filters.command(["no", "no@ColinsharkBot"]))
 def no(bot: ColinSharkBot, msg: Message):
     try:
         msg.reply_text("What did you expect to happen? " + Emoji.MAN_SHRUGGING)
